c1e4ae7ed60bf9bf7e5211157886e9382413e28a
alejorgens <alexandrejorge3@gmail.com>
2017-06-22 14:08:03 -0300

    [#146782393] Adicao da tela de login

---
+++ app/src/main/java/com/expoagro/expoagrobrasil/AnunciosActivity.java
@@ -0,0 +1,13 @@
+package com.expoagro.expoagrobrasil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnunciosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anuncios);
+    }
+}
