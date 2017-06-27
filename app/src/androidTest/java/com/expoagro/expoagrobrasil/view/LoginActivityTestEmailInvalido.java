d9f87fb9544660264c6dd5e81af98430452dc124
SAMIR <samirjosue@gmail.com>
2017-06-27 17:14:06 -0300

    [#146782393] Correção de classes de Teste

--- app/src/androidTest/java/com/expoagro/expoagrobrasil/view/LoginActivityTestEmailInvalido.java
+++ app/src/androidTest/java/com/expoagro/expoagrobrasil/view/LoginActivityTestEmailInvalido.java
@@ -59,7 +59,7 @@ public class LoginActivityTestEmailInvalido {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(5000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
