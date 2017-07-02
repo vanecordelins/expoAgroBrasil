b20df83cf8012fcdd8579286b854040fbd517376
alejorgens <alexandrejorge3@gmail.com>
2017-07-02 18:48:59 -0300

    [#146782131] Adicao da tela de cadastro de usuario
    Adicao da tela de cadastro de usuário e modificação na tela de login para conexão com a nova tela.

---
+++ app/src/main/java/com/expoagro/expoagrobrasil/dao/FirebaseLogin.java
@@ -0,0 +1,63 @@
+package com.expoagro.expoagrobrasil.dao;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import com.expoagro.expoagrobrasil.R;
+import com.expoagro.expoagrobrasil.model.Usuario;
+import com.expoagro.expoagrobrasil.controller.AnunciosActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by Fabricio on 6/22/2017.
+ */
+
+public class FirebaseLogin {
+
+    public static void createFirebaseUser(final Activity activity, FirebaseAuth auth, Usuario user) {
+        auth.createUserWithEmailAndPassword(user.getEmail(), user.getSenha())
+                .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        System.out.println("createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            System.out.println("Authentication failed." + task.getException());
+                        } else {
+                            System.out.println("Authentication sucessul.");
+                            activity.finish();
+                        }
+                    }
+                });
+    }
+
+    public static void firebaseAuthentication(final Activity activity, FirebaseAuth auth, String email, String senha) {
+        auth.signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            // there was an error
+                            Toast.makeText(activity, R.string.error_login, Toast.LENGTH_LONG).show();
+                        } else {
+                            System.out.println("Autorizado.");
+                            Intent it = new Intent(activity, AnunciosActivity.class);
+                            activity.startActivity(it);
+                            activity.finish();
+                        }
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/expoagro/expoagrobrasil/model/Usuario.java
@@ -0,0 +1,76 @@
+package com.expoagro.expoagrobrasil.model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by Fabricio on 6/22/2017.
+ */
+@IgnoreExtraProperties
+public class Usuario {
+
+        private String id;
+        private String nome;
+        private String email;
+        private String telefone;
+        private String cidade;
+        private String senha;
+
+        public Usuario() {
+
+        }
+
+        public Usuario(String nome, String email, String telefone, String cidade, String senha) {
+            this.nome=nome;
+            this.email=email;
+            this.telefone=telefone;
+            this.cidade = cidade;
+            this.senha=senha;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public void setSenha(String senha) {
+            this.senha = senha;
+        }
+
+        public void setTelefone(String telefone) {
+            this.telefone = telefone;
+        }
+
+        public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+        public  String getId() {
+            return id;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public String getSenha() {
+            return senha;
+        }
+
+        public String getTelefone() {
+            return telefone;
+        }
+
+        public String getCidade() { return cidade; }
+
+    }
