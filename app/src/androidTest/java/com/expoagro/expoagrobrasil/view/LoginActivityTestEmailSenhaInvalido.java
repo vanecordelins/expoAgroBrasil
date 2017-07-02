d94a252a13a8c3eca651d5fb5d90871a4aa21b54
SAMIR <samirjosue@gmail.com>
2017-07-01 21:18:22 -0300

    [#146782393] Correção de classes de Teste

---
+++ app/src/androidTest/java/com/expoagro/expoagrobrasil/view/LoginActivityTestEmailSenhaInvalido.java
@@ -0,0 +1,74 @@
+package com.expoagro.expoagrobrasil.view;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.view.WindowManager;
+
+import com.expoagro.expoagrobrasil.R;
+import com.expoagro.expoagrobrasil.controller.LoginActivity;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTestEmailSenhaInvalido {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Before
+    public void unlockScreen() {
+        final LoginActivity activity = mActivityTestRule.getActivity();
+        Runnable wakeUpDevice = new Runnable() {
+            public void run() {
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                        | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                        | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                        | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                        | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+            }
+        };
+        activity.runOnUiThread(wakeUpDevice);
+    }
+
+    @Test
+    public void loginActivityTest() throws Exception{
+        onView(withId(R.id.email)).perform(typeText("didi@didi.com"));
+        closeKeyboard();
+        onView(withId(R.id.password)).perform(typeText("senhateste"));
+        closeKeyboard();
+        onView(withId(R.id.btnEntrar)).perform(click());
+
+        Thread.sleep(3000);
+    }
+
+    public void closeKeyboard() throws Exception {
+        Espresso.closeSoftKeyboard();
+        Thread.sleep(3000);
+    }
+
+}
