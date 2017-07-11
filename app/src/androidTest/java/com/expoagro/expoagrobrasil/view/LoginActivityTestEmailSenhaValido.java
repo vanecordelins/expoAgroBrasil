92743bffcbe8fc6e37f2eec7e084338fa439cd94
SAMIR <samirjosue@gmail.com>
2017-07-11 07:44:42 -0300

    Testes de aceitação

--- app/src/androidTest/java/com/expoagro/expoagrobrasil/view/LoginActivityTestEmailSenhaValido.java
+++ app/src/androidTest/java/com/expoagro/expoagrobrasil/view/LoginActivityTestEmailSenhaValido.java
@@ -26,7 +26,6 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
--- app/src/main/java/com/expoagro/expoagrobrasil/controller/LoginActivity.java
+++ app/src/main/java/com/expoagro/expoagrobrasil/controller/LoginActivity.java
@@ -2,17 +2,12 @@ package com.expoagro.expoagrobrasil.controller;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-
-import android.support.v7.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
