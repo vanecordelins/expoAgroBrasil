5891375226e5dad663d683c65ff51ab3a49e9334
Fabricio Paes <fabriciopaes1@gmail.com>
2017-07-19 11:16:40 -0300

    [#146783267] Cadastro de Produto no banco por foto ou por upload de imagem

--- app/src/main/java/com/expoagro/expoagrobrasil/model/Servico.java
+++ app/src/main/java/com/expoagro/expoagrobrasil/model/Servico.java
@@ -8,8 +8,10 @@ public class Servico extends Anuncio {
 
     private String frequencia;
 
-    public Servico(String id, String nome, String observacao, String descricao, String data, String hora, float valor, String frequencia) {
-        super(id, nome, observacao, descricao, data, hora, valor);
+    public Servico() {}
+
+    public Servico(String nome, String observacao, String descricao, String data, String hora, float valor, String frequencia) {
+        super(nome, observacao, descricao, data, hora, valor);
         this.frequencia = frequencia;
     }
 
