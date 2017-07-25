a7515bff85acc7007d355570610a5f454550ef9e
JoaoBoscoJr <jb_junior@live.com>
2017-07-25 10:14:40 -0300

    [#146783267]Listagem de produtos

--- app/src/main/java/com/expoagro/expoagrobrasil/controller/MenuActivity.java
+++ app/src/main/java/com/expoagro/expoagrobrasil/controller/MenuActivity.java
@@ -1,39 +1,36 @@
 package com.expoagro.expoagrobrasil.controller;
 
-import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.SyncStateContract;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.view.Menu;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.expoagro.expoagrobrasil.Manifest;
 import com.expoagro.expoagrobrasil.R;
-
 import com.expoagro.expoagrobrasil.dao.ProdutoDAO;
 import com.expoagro.expoagrobrasil.dao.UserDAO;
-import com.expoagro.expoagrobrasil.model.Anuncio;
 import com.expoagro.expoagrobrasil.model.Produto;
 import com.expoagro.expoagrobrasil.model.Usuario;
-import com.expoagro.expoagrobrasil.util.GoogleSignIn;
+import com.expoagro.expoagrobrasil.util.Lista;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
@@ -43,10 +40,13 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -60,6 +60,11 @@ public class MenuActivity extends AppCompatActivity
     private String uid;
     private static final int CALL_IMAGE = 12;
     private static final int CALL_CAMERA = 14;
+    private RecyclerView recyclerView;
+    private DatabaseReference myref;
+    private DatabaseReference myref2;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,10 +82,9 @@ public class MenuActivity extends AppCompatActivity
 
         uid = "";
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        Button btn_sair = (Button) findViewById(R.id.btn_sair);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -88,47 +92,104 @@ public class MenuActivity extends AppCompatActivity
                 .build();
 
         mGoogleApiClient = new GoogleApiClient.Builder(MenuActivity.this)
-                .enableAutoManage(MenuActivity.this /* FragmentActivity */, MenuActivity.this /* OnConnectionFailedListener */)
+                .enableAutoManage(MenuActivity.this, MenuActivity.this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
+        // ----------------------------------RecyclerView-----------------------------------------------------------
 
-        btn_sair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GoogleSignIn.signOut(MenuActivity.this, mGoogleApiClient);
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
 
-        Button btn_photo = (Button) findViewById(R.id.photo_bt);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        myref = FirebaseDatabase.getInstance().getReference("Produto");
+            FirebaseRecyclerAdapter<Lista, ListaViewHolder> recyclerAdapter = new FirebaseRecyclerAdapter<Lista, ListaViewHolder>(
+                    Lista.class,
+                    R.layout.linha,
+                    ListaViewHolder.class,
+                    myref
 
-        btn_photo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (ActivityCompat.checkSelfPermission(MenuActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MenuActivity.this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, CALL_IMAGE);
-                } else {
-                    Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    startActivityForResult(galleryIntent, CALL_IMAGE);
+
+            ) {
+
+                @Override
+                protected void populateViewHolder(ListaViewHolder viewHolder, Lista model, int position) {
+
+                    final String key = getRef(position).getKey();
+
+                    viewHolder.setCategoria(model.getCategoria());
+                    viewHolder.setData(model.getData());
+                    viewHolder.setValor(model.getValor());
+                    viewHolder.setFoto(model.getFoto());
+                    viewHolder.setNome(model.getNome());
+
+
+                    viewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Toast.makeText(MenuActivity.this, key, Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+
                 }
-            }
-        });
 
-        Button btn_tirarfoto = (Button) findViewById(R.id.tirar_foto);
 
-        btn_tirarfoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (ActivityCompat.checkSelfPermission(MenuActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MenuActivity.this, new String[]{android.Manifest.permission.CAMERA}, CALL_CAMERA);
-                } else {
-                    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                    startActivityForResult(intent, CALL_CAMERA);
-                }
-            }
-        });
+            };
 
+            recyclerView.setAdapter(recyclerAdapter);
     }
+
+    public static class ListaViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+        TextView textView_nome;
+        TextView textView_data;
+        TextView textView_valor;
+        TextView textView_categoria;
+        ImageView imageView;
+
+        public ListaViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+            textView_nome = (TextView) itemView.findViewById(R.id.nome);
+            textView_data = (TextView) itemView.findViewById(R.id.data);
+            textView_valor = (TextView) itemView.findViewById(R.id.valor);
+            textView_categoria = (TextView) itemView.findViewById(R.id.categoria);
+            imageView = (ImageView) itemView.findViewById(R.id.foto);
+        }
+
+
+        public void setNome(String nome) {
+            textView_nome.setText(nome);
+        }
+
+        public void setData(String data) {
+            textView_data.setText(data);
+        }
+
+        public void setValor(String valor) {
+            textView_valor.setText(valor);
+        }
+
+        public void setCategoria(String categoria) {
+            textView_categoria.setText(categoria);
+        }
+
+
+        public void setFoto(List<String> foto) {
+            if (foto == null) {
+            } else {
+                Picasso.with(mView.getContext())
+                        .load(foto.get(0))
+                        .resize(50,50)
+                        .into(imageView);
+            }
+        }
+    }
+
+//---------------------------------------------------------------------------------------
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -136,27 +197,16 @@ public class MenuActivity extends AppCompatActivity
         final TextView nomeUsuarioLogado = (TextView) findViewById(R.id.menu_nome);
         final TextView emailUsuarioLogado = (TextView) findViewById(R.id.menu_email);
 
-        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
             uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         }
-
-        emailUsuarioLogado.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (FirebaseAuth.getInstance().getCurrentUser() == null) {
-                    Intent it = new Intent(MenuActivity.this, LoginActivity.class);
-                    startActivity(it);
-                    finish();
-                }
-            }
-        });
 
 
         UserDAO.getReference().addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot user : dataSnapshot.getChildren()) {
-                    if(user.getKey().equals(uid)){
+                for (DataSnapshot user : dataSnapshot.getChildren()) {
+                    if (user.getKey().equals(uid)) {
                         Usuario target = user.getValue(Usuario.class);
                         nomeUsuarioLogado.setText(target.getNome());
                         emailUsuarioLogado.setText(target.getEmail());
@@ -192,7 +242,7 @@ public class MenuActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.menu_meu_perfil) {
-            if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+            if (FirebaseAuth.getInstance().getCurrentUser() != null) {
                 Intent telaVisualizar = new Intent(MenuActivity.this, VisualizarUsuarioActivity.class);
                 startActivity(telaVisualizar);
                 finish();
@@ -202,7 +252,7 @@ public class MenuActivity extends AppCompatActivity
                 finish();
             }
         } else if (id == R.id.menu_novo_anuncio) {
-            if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+            if (FirebaseAuth.getInstance().getCurrentUser() != null) {
                 System.out.println("MENU NOVO ANUNCIOS"); // Ja esta logado
             } else {
                 Intent telaLogin = new Intent(MenuActivity.this, LoginActivity.class);
@@ -210,7 +260,7 @@ public class MenuActivity extends AppCompatActivity
                 finish();
             }
         } else if (id == R.id.menu_meus_anuncios) {
-            if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+            if (FirebaseAuth.getInstance().getCurrentUser() != null) {
                 System.out.println("MENU MEUS FAVORITOS"); // Ja esta logado
             } else {
                 Intent telaLogin = new Intent(MenuActivity.this, LoginActivity.class);
@@ -244,7 +294,7 @@ public class MenuActivity extends AppCompatActivity
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == CALL_IMAGE) {
-                // If request is cancelled, the result arrays are empty.
+            // If request is cancelled, the result arrays are empty.
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(galleryIntent, CALL_IMAGE);
@@ -292,9 +342,9 @@ public class MenuActivity extends AppCompatActivity
                 });
 
             }
-        } else if(resultCode != RESULT_OK) {
+        } else if (resultCode != RESULT_OK) {
             System.out.println("Cancelado pelo usuário");
-        } else  if(requestCode == CALL_IMAGE) {
+        } else if (requestCode == CALL_IMAGE) {
             Cursor cursor = getContentResolver().query(intent.getData(), null, null, null, null);
             cursor.moveToFirst();
             int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
@@ -317,14 +367,14 @@ public class MenuActivity extends AppCompatActivity
                     final List<String> lista = new ArrayList<String>();
                     lista.add(downloadUrl.toString());
                     uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                    Produto produto = new Produto("Boi", "obs", "desc", "data", "hora", 100, "categoria", lista );
+                    Produto produto = new Produto("Boi", "obs", "desc", "data", "hora", 100, "categoria", lista);
                     ProdutoDAO pdao = new ProdutoDAO();
                     pdao.save(produto, uid);
                 }
             });
-        //    Glide.with(this /* context */)
-           //         .load("https://firebasestorage.googleapis.com/v0/b/expoagro-brasil.appspot.com/o/armadefogo.png?alt=media&token=35607849-7763-4967-a846-6ef775ae16ec")
-           //         .into((ImageView) findViewById(R.id.imageView));
+            //    Glide.with(this /* context */)
+            //         .load("https://firebasestorage.googleapis.com/v0/b/expoagro-brasil.appspot.com/o/armadefogo.png?alt=media&token=35607849-7763-4967-a846-6ef775ae16ec")
+            //         .into((ImageView) findViewById(R.id.imageView));
         }
     }
 }
